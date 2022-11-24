c6fabbbd3911087b521640411bc6542b62e0f23b
Celebi <wmd212@wiso.ad.uni-koeln.de>
2022-11-24 18:55:14 +0100

    Added: search user + search followers for "elifcance" account

--- code/InfHashtagSearch.R
+++ code/InfHashtagSearch.R
@@ -1,2 +1,8 @@
 rstats_tweets <- search_tweets(q= "#EmeklilikteYasaTakilanlar", n=Inf)
 rstatsEYT_tweets <- search_tweets(q= "#EYT", n=Inf)
+
+textdata <- rstats_tweets$text #load all text 
+tweet_corpus <- Corpus(textdata) #create corpus
+user_ec <- search_users("elifcance") #search user
+followers_ec <- get_followers("elifcance") #get followers of an account
+
